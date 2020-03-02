da5b1dd8b437e27587ac20de92a218c5adfc40ac
okovtun <okovtun@itstep.org>
2020-03-02 21:06:47 +0200

    Arr2D

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+const int ROWS = 10;
+const int COLS = 10;
+
 void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
+
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
+
 void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void main()
 {
@@ -13,14 +24,49 @@ void main()
 	Print(arr, n);
 	Sort(arr, n);
 	Print(arr, n);
+
+	const int m = 8;
+	double brr[m];
+	FillRand(brr, m);
+	Print(brr, m);
+	Sort(brr, m);
+	Print(brr, m);
+
+	
+	int A[ROWS][COLS];
+	FillRand(A, ROWS, COLS);
+	Print(A, ROWS, COLS);
+	Sort(A, ROWS, COLS);
+	Print(A, ROWS, COLS);
 }
 
 void FillRand(int arr[], const int n)
 {
-	cout << typeid(arr).name() << endl;
+	//cout << typeid(arr).name() << endl;
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
+	}
+}
+
+void FillRand(double arr[], const int n)
+{
+	//cout << typeid(arr).name() << endl;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = double(rand() % 100)/10;
+		//arr[i] /= 10;
+	}
+}
+
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
 	}
 }
 
@@ -33,11 +79,32 @@ void Print(int arr[], const int n)
 	cout << endl;
 }
 
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] <<"\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
 void Sort(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = i+1; j < n; j++)
 		{
 			if (arr[j] < arr[i])
 			{
@@ -47,4 +114,45 @@ void Sort(int arr[], const int n)
 			}
 		}
 	}
+}
+
+void Sort(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i+1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				double buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+
+void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	int iterations = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = 0; k < ROWS; k++)
+			{
+				for (int l = 0; l < COLS; l++)
+				{
+					if (arr[k][l] < arr[i][j])
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+					}
+					iterations++;
+				}
+			}
+		}
+	}
+	cout << "Number of iterations: " << iterations << endl;
 }
